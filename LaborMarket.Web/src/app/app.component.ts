344bf824d2d3ad2c51fb4a04a6f72ecf6b4ac2de
Svetoslav Taterski <svetoslav0247@gmail.com>
2025-04-24 21:34:34 +0300

    Creating back-end API project

--- LaborMarket.Web/src/app/app.component.ts
+++ LaborMarket.Web/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { CardComponent } from "./components/card/card.component";
-import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CardComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
