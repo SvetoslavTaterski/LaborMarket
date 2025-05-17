45c6ef5cc6fe43001117bd2bebb7cbd305e8d9bd
Svetoslav Taterski <svetoslav0247@gmail.com>
2025-05-17 13:59:59 +0300

    Adding pagination on user table and styling it a bit more

--- LaborMarket.Web/src/app/components/pages/workers-page/workers-page.component.ts
+++ LaborMarket.Web/src/app/components/pages/workers-page/workers-page.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '../../../services/user.service';
 import { UserDataModel } from '../../../models/user-model';
+import { HeaderComponent } from "../../header/header.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-workers-page',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
+  imports: [MatTableModule, CommonModule, HeaderComponent, FooterComponent, MatPaginator],
   templateUrl: './workers-page.component.html',
   styleUrl: './workers-page.component.scss',
 })
 export class WorkersPageComponent implements OnInit {
-  public userData: UserDataModel[] = [];
+  public userData= new MatTableDataSource<UserDataModel>([]);
   public displayedColumns: string[] = ['firstName', 'lastName', 'email', 'createdAt'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private userService: UserService) {}
 
@@ -21,13 +27,17 @@ export class WorkersPageComponent implements OnInit {
 
     this.userService.getAllUsers().subscribe({
       next: (response) => {
-        this.userData = response;
+        this.userData.data = response;
       },
       error: (err) => {
         console.error('Failed to load users:', err);
       }
     });
     
+  }
+
+  ngAfterViewInit() {
+    this.userData.paginator = this.paginator;
   }
 
 }
