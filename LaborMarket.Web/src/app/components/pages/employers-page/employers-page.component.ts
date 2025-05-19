012fb1fdcd6621c1a1534e263e9ef78cb5e77461
Svetoslav Taterski <svetoslav0247@gmail.com>
2025-05-19 21:20:32 +0300

    Creating employers page and styiling it

--- LaborMarket.Web/src/app/components/pages/employers-page/employers-page.component.ts
+++ LaborMarket.Web/src/app/components/pages/employers-page/employers-page.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployerService } from '../../../services/employer.service';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { EmployerDataModel } from '../../../models/employer-model';
+import { MatPaginator } from '@angular/material/paginator';
+import { HeaderComponent } from "../../header/header.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-employers-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, MatPaginator, MatTableModule],
   templateUrl: './employers-page.component.html',
   styleUrl: './employers-page.component.scss',
 })
 export class EmployersPageComponent implements OnInit {
   public employersData = new MatTableDataSource<EmployerDataModel>([]);
+  public displayedColumns: string[] = ['companyName', 'companyEmail', 'companyPhone'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private employerService: EmployerService) {}
 
   ngOnInit(): void {
     this.employerService.getAllEmployers().subscribe({
-        next: (response) => {
-          this.employersData.data = response;
-        },
-        error: (err) => {
-          console.error('Failed to load jobs:', err);
-        }
-      });
+      next: (response) => {
+        this.employersData.data = response;
+      },
+      error: (err) => {
+        console.error('Failed to load employers:', err);
+      },
+    });
   }
+
+  ngAfterViewInit() {
+      this.employersData.paginator = this.paginator;
+    }
 }
