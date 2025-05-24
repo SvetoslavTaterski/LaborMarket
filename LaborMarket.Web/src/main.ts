d670d020d8400592bd1e3ee9e67773feed57b20f
Svetoslav Taterski <svetoslav0247@gmail.com>
2025-05-24 16:04:04 +0300

    Add login page component with styling and routing

--- LaborMarket.Web/src/main.ts
+++ LaborMarket.Web/src/main.ts
@@ -6,12 +6,14 @@ import { WorkersPageComponent } from './app/components/pages/workers-page/worker
 import { PositionsPageComponent } from './app/components/pages/positions-page/positions-page.component';
 import { EmployersPageComponent } from './app/components/pages/employers-page/employers-page.component';
 import { provideHttpClient } from '@angular/common/http';
+import { LoginPageComponent } from './app/components/pages/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent }, // Default route
   { path: 'workers', component: WorkersPageComponent },
   { path: 'positions', component: PositionsPageComponent},
   { path: 'employers', component: EmployersPageComponent},
+  { path: 'login', component: LoginPageComponent}
 ];
 
 bootstrapApplication(AppComponent, {
